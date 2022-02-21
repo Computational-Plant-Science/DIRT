d94d250a0e8d41d6d489e96bca61f8deefe36ae9
w-bonelli <wpb3hw@virginia.edu>
2022-02-21 08:59:31 -0500

    update shim script & plantit.yaml

--- shim.sh
+++ shim.sh
@@ -1,3 +1,15 @@
 python3 /opt/DIRT/main.py "$INPUT" $INDEX $MASK_THRESHOLD $EXCISED_ROOTS 1 1 25.1 0 0 0 $WORKDIR /opt/DIRT/traits.csv
-cat $INDEX/output.csv > "$WORKDIR/results$INDEX.csv"
+
+# copy individual CSVs to working dir and concatenate CSV output into a single file
+results="$WORKDIR/output.$INDEX.csv"
+mv "$INDEX/output.csv" $results
+all_results="$WORKDIR/results.csv"
+if [ -s $all_results ]
+then
+        tail -qn +2 $results >> $all_results
+else
+        cat $results > $all_results
+fi
+
+# copy all pngs from nested output dirs to working dir
 find $INDEX -type f -name "*.png" -exec cp {} $WORKDIR \;
